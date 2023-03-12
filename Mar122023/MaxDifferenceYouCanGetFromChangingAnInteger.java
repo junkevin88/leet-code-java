9f765be9c511972b7b44d5e269ad6064846635d5
junkevin88 <junkevin88@gmail.com>
2023-03-12 22:16:02 +0700

    add problem 12032023

---
+++ Mar122023/MaxDifferenceYouCanGetFromChangingAnInteger.java
@@ -0,0 +1,71 @@
+package Mar122023;
+
+class MaxDifferenceYouCanGetFromChangingAnInteger {
+    public int maxDiff(int num) {
+
+        /*
+        Given an integer num. You will apply the following steps exactly two times:
+        Pick a digit x (0 <= x <= 9).
+        Pick another digit y (0 <= y <= 9). The digit y can be equal to x.
+        Replace all the occurrences of x in the decimal representation of num by y.
+        The new integer cannot have any leading zeros, also the new integer cannot be 0.
+        Let a and b be the results of applying the operations to num the first and second times, respectively.
+        Return the max difference between a and b.
+        Example 1:
+        Input: num = 555
+        Output: 888
+        Explanation: The first time pick x = 5 and y = 9 and store the new integer in a.
+        The second time pick x = 5 and y = 1 and store the new integer in b.
+        We have now a = 999 and b = 111 and max difference = 888
+        Example 2:
+        Input: num = 9
+        Output: 8
+        Explanation: The first time pick x = 9 and y = 9 and store the new integer in a.
+        The second time pick x = 9 and y = 1 and store the new integer in b.
+        We have now a = 9 and b = 1 and max difference = 8
+        Example 3:
+        Input: num = 123456
+        Output: 820000
+        Example 4:
+        Input: num = 10000
+        Output: 80000
+        Example 5:
+        Input: num = 9288
+        Output: 8700
+        Constraints:
+        1 <= num <= 10^8
+         */
+
+//        answer:
+
+        String s = String.valueOf(num);
+        char[] c = s.toCharArray();
+        int i = 0;
+        while (c[i] == '9') {
+            i++;
+        }
+        char x = c[i];
+        char y = '9';
+        for (int j = 0; j < c.length; j++) {
+            if (c[j] == x) {
+                c[j] = y;
+            }
+        }
+        int a = Integer.parseInt(String.valueOf(c));
+        i = 0;
+        while (c[i] == '1' || c[i] == '0') {
+            i++;
+        }
+        x = c[i];
+        y = '1';
+        for (int j = 0; j < c.length; j++) {
+            if (c[j] == x) {
+                c[j] = y;
+            }
+        }
+        int b = Integer.parseInt(String.valueOf(c));
+        return a - b;
+
+    }
+
+}
