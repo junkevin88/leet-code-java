0f3f8a1900e97b47a9eee59393a2f5994ac2003d
junkevin88 <junkevin88@gmail.com>
2023-03-14 23:01:59 +0700

    add file 14032023

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,7 @@
 0Mar122023/KidWithTheGreatestNumberOfCandies.java,a\c\acef4539289e5c59f59f5a40097b7b15aa0d043a
 j
 :Mar122023/MaxDifferenceYouCanGetFromChangingAnInteger.java,2\c\2cdf947e9a85456177326f981f828f8c08ced0b6
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- mar132023/SymmetricTree.java
+++ mar132023/SymmetricTree.java
@@ -1,18 +1,6 @@
 package mar132023;
 
 
-public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
 
 class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
