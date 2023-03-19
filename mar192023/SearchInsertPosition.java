11496bda1a3682f0c79de2fd727df52807d5adb1
junkevin88 <junkevin88@gmail.com>
2023-03-19 22:49:39 +0700

    add problem 19 mar 2023

---
+++ mar192023/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package mar192023;
+
+class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
