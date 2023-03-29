b9ea2f2d253b3ac836061c576685bf2baab14dee
junkevin88 <junkevin88@gmail.com>
2023-03-29 12:12:10 +0700

    add problem 29032023

---
+++ mar292023/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,23 @@
+package mar292023;
+
+public class TwoSumIIInputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int[] result = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                result[0] = left + 1;
+                result[1] = right + 1;
+                return result;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
