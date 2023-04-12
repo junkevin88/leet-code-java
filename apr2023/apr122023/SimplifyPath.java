738123aaadc8a7668a56929cb2d71462c64c0ebe
junkevin88 <junkevin88@gmail.com>
2023-04-12 13:23:16 +0700

    add problem 09042023

---
+++ apr2023/apr122023/SimplifyPath.java
@@ -0,0 +1,22 @@
+package apr2023.apr122023;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Deque<String> dir_stack = new ArrayDeque<>();
+        for (String dir : path.split("/")) {
+            if (!dir_stack.isEmpty() && dir.equals("..")) {
+                dir_stack.removeLast();
+            } else if (!dir.equals(".") && !dir.equals("") && !dir.equals("..")) {
+                dir_stack.addLast(dir);
+            }
+        }
+        StringBuilder simplified_path = new StringBuilder();
+        for (String dir : dir_stack) {
+            simplified_path.append("/").append(dir);
+        }
+        return simplified_path.length() == 0 ? "/" : simplified_path.toString();
+    }
+}
