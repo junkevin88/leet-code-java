9f9316bcca23505659d332feec04d606be901ab2
junkevin88 <junkevin88@gmail.com>
2023-04-20 09:29:46 +0700

    add problem 20042023

---
+++ apr2023/apr202023/TreeNode.java
@@ -0,0 +1,14 @@
+package apr2023.apr202023;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
