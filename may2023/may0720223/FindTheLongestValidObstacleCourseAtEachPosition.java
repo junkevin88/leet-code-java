ee9d23639eec85815e175378f25299a628b66165
junkevin88 <junkevin88@gmail.com>
2023-05-07 10:03:55 +0700

    add problem 07052023

---
+++ may2023/may0720223/FindTheLongestValidObstacleCourseAtEachPosition.java
@@ -0,0 +1,26 @@
+package may2023.may0720223;
+
+public class FindTheLongestValidObstacleCourseAtEachPosition {
+    public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+
+        int n = obstacles.length;
+        int length = 0;
+        int[] result = new int[n];
+        int[] increasingSubseq = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int left = 0, right = length;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (increasingSubseq[mid] <= obstacles[i])
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+            result[i] = left + 1;
+            if (length == left)
+                length++;
+            increasingSubseq[left] = obstacles[i];
+        }
+        return result;
+    }
+}
