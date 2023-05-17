d5983d47918604fc53f940725e81186813cf18f6
junkevin88 <junkevin88@gmail.com>
2023-05-17 13:32:39 +0700

    add problem 17052023

---
+++ may2023/may172023/MaximumTwinSumOfALinkedList.java
@@ -0,0 +1,32 @@
+package may2023.may172023;
+
+public class MaximumTwinSumOfALinkedList {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int maxVal = 0;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode nextNode, prev = null;
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        while (prev != null) {
+            maxVal = Math.max(maxVal, head.val + prev.val);
+            prev = prev.next;
+            head = head.next;
+        }
+
+        return maxVal;
+
+    }
+
+}
